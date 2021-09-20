02b4e7e3c5122b888e30ad80385715da2dd1caff
nidhal <nidhal.jelassi@fsegt.utm.tn>
2021-09-20 20:35:42 +0100

    step 2

--- src/app/detail-course/detail-course.page.ts
+++ src/app/detail-course/detail-course.page.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-course.page.scss'],
 })
 export class DetailCoursePage implements OnInit {
+  selectedCourse = {
+    id: 1,
+    title: 'Angular',
+    author: 'Nidhal Jelassi',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/480px-Angular_full_color_logo.svg.png',
+    keywords: ['Components', 'Directives', 'Pipes'],
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
