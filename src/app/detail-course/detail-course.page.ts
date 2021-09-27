d0c9489d9f6e08f266ca72f0c91a63dd50b2574e
nidhal <nidhal.jelassi@fsegt.utm.tn>
2021-09-27 18:44:46 +0100

    step 3

--- src/app/detail-course/detail-course.page.ts
+++ src/app/detail-course/detail-course.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ListCoursesService } from '../list-courses.service';
 
 @Component({
   selector: 'app-detail-course',
@@ -6,15 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-course.page.scss'],
 })
 export class DetailCoursePage implements OnInit {
-  selectedCourse = {
-    id: 1,
-    title: 'Angular',
-    author: 'Nidhal Jelassi',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/480px-Angular_full_color_logo.svg.png',
-    keywords: ['Components', 'Directives', 'Pipes'],
-  };
-  constructor() {}
+  selectedCourse;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private courseServ: ListCoursesService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(
+      (p: ParamMap) => {
+        console.log(p.get('id'));
+        this.selectedCourse = this.courseServ.getCourseById(p.get('id'));
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
