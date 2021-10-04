ed60877749e836a579c0190db7094d691b893719
nidhal <nidhal.jelassi@fsegt.utm.tn>
2021-10-04 19:46:41 +0100

    add httpClient

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ListCoursesService } from '../list-courses.service';
 
@@ -8,9 +9,24 @@ import { ListCoursesService } from '../list-courses.service';
 })
 export class HomePage implements OnInit {
   tab = [];
-  constructor(private listCourseSer: ListCoursesService) {}
+  constructor(
+    private listCourseSer: ListCoursesService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.tab = this.listCourseSer.getAllCourses();
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe(
+      (result) => {
+        console.log(result[1]['address']['geo']['lat']);
+        console.log(result[1].address.geo.lat);
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        console.log('Flux terminé !');
+      }
+    );
   }
 }
